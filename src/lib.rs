1642bcf9a94f08adc04c074bcac546b1b69ffdd2
Константин Бедрицкий <kmb2@tpu.ru>
2020-08-04 23:05:38 +0600

    Tests are now compiling

---
+++ src/lib.rs
@@ -0,0 +1,19 @@
+pub struct KvStore;
+
+impl KvStore {
+    pub fn new() -> Self {
+        unimplemented!()
+    }
+
+    pub fn set(&mut self, key: String, value: String) {
+        unimplemented!()
+    }
+
+    pub fn get(&self, key: String) -> Option<String> {
+        unimplemented!()
+    }
+
+    pub fn remove(&mut self, key: String) {
+        unimplemented!()
+    }
+}
